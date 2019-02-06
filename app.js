0e44aec2c539914eaf70e54d57fa0dca3d10e274
Brian Michael Witt <bmwitt1234@Brians-MBP-2.attlocal.net>
2019-02-05 19:01:50 -0500

    added features

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 $('#view').on('click', function(){
     $('#display').empty();
+    console.log(employeeList)
     for(let i = 0; i < employeeList.length; i++) {
         $('#display')
         .append(
@@ -33,7 +34,75 @@ $('#add').on('click', function(){
 
         employeeList.push(newEmployee);
     
+
+
     })
 })
 
+$('#update').on('click', function(){
+    console.log("update")
+    $('#display').empty();
+    $('#display').append(`
+        <div class="inputForm">
+        <label for="name">Update/Change Information by Name</label>
+        <input id="name" placeholder="name"/>
+        <input id="officeNum" placeholder="office number"/>
+        <input id="phoneNum" placeholder="phone number"/>
+        <button id="submit">submit</button>
+        </div>
+    `)
 
+    $('#submit').on('click', function(){
+    
+            var name= $('#name').val()
+            var officeNum=$('#officeNum').val()
+            var phoneNum= $('#phoneNum').val()
+    
+
+        for (let i=0; i<employeeList.length; i++) {
+            if(employeeList[i].name === name){
+                employeeList[i].officeNum = officeNum
+                employeeList[i].phoneNum = phoneNum
+
+            }
+        
+        }
+    
+    
+
+        
+    })
+})
+
+$('#delete').on('click', function(){
+    $('#display').empty();
+    $('#display').append(`
+        <div class="inputForm">
+        <input id="name" placeholder="name"/>
+       
+        <button id="submit">submit</button>
+        </div>
+    `)
+
+    $('#submit').on('click', function(){
+        
+         var name = $('#name').val()
+          var index
+            
+        for (let i=0; i<employeeList.length; i++) {
+            if(employeeList[i].name === name){
+               index = i
+
+            }
+        
+        }
+        console.log(index)
+        employeeList.splice(index,1)
+        console.log(employeeList)
+
+       
+    
+
+        
+    })
+})
